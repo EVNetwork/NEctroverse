69c6621f5eda6a6d09ed153b96da7c66bf22ea29
Admin Necro <stephen.knill@live.com.au>
2014-01-17 17:07:38 +1030

    Test 2.

--- http/http.c
+++ http/http.c
@@ -356,7 +356,11 @@ void httpString( ReplyDataPtr rd, char *text ) {
 	int buf_max = (rd->cache.buf_len - rd->cache.off);
 	int buf_len;
 	
-buf_len = text ? strlen( text ) : 0;
+if( text == NULL ) {
+	return;
+} else {
+	buflen = strlen( text );
+}
 	
 if( ( buf_max - buf_len ) < 0 ) {
 	rd->cache.buf = buffer_realloc( &rd->cache, 2, buf_len, &buf_max );
